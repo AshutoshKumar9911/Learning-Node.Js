d767109a1cc21a536e4b5911ec618727f412457a
ashutosh <ashutoshk54376@gmail.com>
2026-07-19 13:15:53 +0530

    Add sending_Response.js

---
+++ Request and Response/Sending_Response.js
@@ -0,0 +1,18 @@
+// Simple Node Server
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title> My First Page </title></head> ');
+  res.write('<body><h1>Ashutosh Kumar / Preaparing for Gate</h1></body>')
+  res.write('</html>');
+  res.end();
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=>{
+  console.log(`Server running at http://localhost:${PORT}`)
+});
